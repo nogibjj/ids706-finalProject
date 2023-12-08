17dcc6b5da6fe340f8e915b77d498460d99f597a
Asterisk <44215173+casnz1601@users.noreply.github.com>
2023-12-08 18:56:57 +0000

    update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ async def root():
 @app.get("/district_info/{district_name}")
 async def get_district_info(district_name: str):
     """Return educational metrics for a specified school district"""
-    decoded_district_name = unquote(district_name)  # Decodes URL-encoded strings
+    decoded_district_name = unquote(district_name)
     return db.get_district_info(decoded_district_name)
 
 @app.get("/all_districts_info")
@@ -22,7 +22,6 @@ async def get_all_districts_info():
     """Return summary info about all school districts"""
     return db.get_all_districts_info()
 
-# Additional endpoints can be added here as needed
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host="0.0.0.0")
---
+++ test_app.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "Welcome to the Educational Metrics Service!"
+
+def test_get_district_info():
+    response = client.get("/district_info/East%20Springs%20District%201")
+    assert response.status_code == 200
+
+def test_get_all_districts_info():
+    response = client.get("/all_districts_info")
+    assert response.status_code == 200
