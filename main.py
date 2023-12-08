0a0dcb569801092b04756dc11532f7634221d5ad
Asterisk <44215173+casnz1601@users.noreply.github.com>
2023-12-08 21:34:25 +0000

    update

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ db = DB()
 
 @app.get("/")
 async def root():
-    """Welcome message on homepage"""
     return "Welcome to the Educational Metrics Service!"
 
 @app.get("/district_info/{district_name}")
 async def get_district_info(district_name: str):
-    """Return educational metrics for a specified school district"""
     decoded_district_name = unquote(district_name)
     return db.get_district_info(decoded_district_name)
 
 @app.get("/all_districts_info")
 async def get_all_districts_info():
-    """Return summary info about all school districts"""
     return db.get_all_districts_info()
 
 
